390cc0ad199184bc56c031446247e86266069a65
oguzcoll <oguzcolak2000@gmail.com>
2023-11-02 19:59:51 +0300

    create slug and single course controller

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -28,3 +28,17 @@ exports.getAllCourses = async (req, res) => {
     });
   }
 };
+exports.getCourse = async (req, res) => {
+  try {
+    const course = await Course.findOne({ slug: req.params.slug });
+    res.status(200).render("course", {
+      course,
+      page_name: "courses",
+    });
+  } catch (error) {
+    res.status(201).json({
+      status: "fail",
+      error,
+    });
+  }
+};
