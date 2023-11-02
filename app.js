3c5ec79bc0a1f090763aa850afe756ea3d02b63f
oguzcoll <oguzcolak2000@gmail.com>
2023-11-02 18:43:45 +0300

    course model created

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute");
+require("dotenv").config();
 
 const app = express();
+
+// connect db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Mongodb Connected");
+  });
 
 //Template Engine
 app.set("view engine", "ejs");
@@ -11,6 +24,7 @@ app.use(express.static("public"));
 
 //Routes
 app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
